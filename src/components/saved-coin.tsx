a0e5f11396a07326bd33fc27a2fe29e1649632e0
cavishek39 <cavishek39@gmail.com>
2022-12-07 20:55:00 +0530

    account page and saved coins ui built

--- src/components/saved-coin.tsx
+++ src/components/saved-coin.tsx
@@ -1,14 +1,52 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedCoin = () => {
+  const [coins, setCoins] = useState([]);
+
   return (
     <div>
-      <p>
-        You don't have any coins saved. Please save a coin to add it to watch
-        list.
-        <Link href={"/"}>Click here</Link>
-      </p>
+      {coins.length === 0 ? (
+        <p>
+          You don't have any coins saved. Please save a coin to add it to watch
+          list.
+          <Link href={"/"}>Click here</Link>
+        </p>
+      ) : (
+        <table className="w-full border-collapse text-center">
+          <thead>
+            <tr className="border-b">
+              <th className="px-4">Rank #</th>
+              <th className="text-left">Coin</th>
+              <th className="text-left">Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <tr key={coin?.id} className="h-[60px] overflow-hidden">
+                <td>{coin?.rank}</td>
+                <td>
+                  <Link href={`/coin/${coin?.id}`}>
+                    <div className="flex items-center">
+                      <img src={coin?.image} alt="/" className="mr-4 w-8" />
+                      <div>
+                        <p className="hidden sm:table-cell">{coin?.name}</p>
+                        <p className="text-left text-sm text-gray-500">
+                          {coin?.symbol?.toUpperCase()}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                </td>
+                <td className="pl-8">
+                  <AiOutlineClose className="cursor-pointer" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -3,21 +3,23 @@ import SavedCoin from "../components/saved-coin";
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className="mx-auto max-w-[1140px]">
+      <div className="rounded-div my-12 flex items-center justify-between py-8">
         <div>
-          <h1>Account</h1>
+          <h1 className="text-2xl font-bold">Account</h1>
           <div>
             <p>Welcome, User</p>
           </div>
         </div>
         <div>
-          <button>Sign Out</button>
+          <button className="rounded-2xl border px-6 py-2 shadow-lg hover:shadow-2xl">
+            Sign Out
+          </button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Watch List</h1>
+      <div className="rounded-div my-12 flex items-center justify-between py-8">
+        <div className="min-h-[300px] w-full">
+          <h1 className="py-4 text-2xl font-bold">Watch List</h1>
           <SavedCoin />
         </div>
       </div>
