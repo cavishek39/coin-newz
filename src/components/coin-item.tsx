13512da64673513b9fb1d586b8e22cf503398581
cavishek39 <cavishek39@gmail.com>
2022-12-08 19:42:04 +0530

    saved coins to user's watchlist

--- src/components/coin-item.tsx
+++ src/components/coin-item.tsx
@@ -1,21 +1,50 @@
 import Link from "next/link";
-import React from "react";
-import { AiOutlineStar } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { Sparklines, SparklinesLine } from "react-sparklines";
 import { Coin } from "../types/coin";
+import { useUserAuth } from "../context/AuthContext";
+import { db } from "../config";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 
 type CoinItemProps = {
   coin: Coin;
 };
 
 const CoinItem = ({ coin }: CoinItemProps) => {
+  const { user } = useUserAuth();
+  const [savedCoin, setSavedCoin] = useState<boolean>(false);
+
+  const coinPath = doc(db, "users", `${user?.email}`);
+
+  const saveCoin = async () => {
+    if (user?.email) {
+      setSavedCoin(true);
+      await updateDoc(coinPath, {
+        watchList: arrayUnion({
+          id: coin?.id,
+          name: coin?.name,
+          image: coin?.image,
+          rank: coin?.market_cap_rank,
+          symbol: coin?.symbol,
+        }),
+      });
+    } else {
+      alert("Please sign in first to save a coin in your watch list!");
+    }
+  };
+
   return (
     <tr
       key={coin.market_cap_rank?.toString()}
       className="h-[80px] overflow-hidden border-b"
     >
-      <td>
-        <AiOutlineStar />
+      <td onClick={saveCoin}>
+        {savedCoin ? (
+          <AiFillStar className="cursor-pointer" color="gold" />
+        ) : (
+          <AiOutlineStar className="cursor-pointer" />
+        )}
       </td>
       <td>{coin.market_cap_rank}</td>
       <td>
